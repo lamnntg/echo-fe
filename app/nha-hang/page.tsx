fcfda8fbadf4275ab3c403016e4c4d20a32ba3db
nguyen-son762 <nguyenson7620000@gmail.com>
2024-04-20 14:36:16 +0700

    add dât

--- app/nha-hang/page.tsx
+++ app/nha-hang/page.tsx
@@ -16,7 +16,7 @@ const page = () => {
         <ChevronRight />
         <span>Nhà hàng</span>
       </div>
-      <div className="mt-10 grid grid-cols-3 gap-3">
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
         {data.map((item) => (
           <div key={item.slug}>
             <Link
